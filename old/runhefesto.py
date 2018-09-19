3f9ff89b43a5c3e1acb9cf259777d64cd0bfd6d2
Scott Hull <ScottHull@users.noreply.github.com>
2018-09-19 10:20:07 -0400

    Add files via upload

--- old/runhefesto.py
+++ old/runhefesto.py
@@ -18,49 +18,55 @@ def cleanup():
 
 if __name__ == "__main__":
 
-    if os.path.exists("HeFESTo_Output_Files"):
-        shutil.rmtree("HeFESTo_Output_Files")
-    os.mkdir(os.getcwd() + "/HeFESTo_Output_Files")
-
     dirname = None
     print("\n\n\nWhat do you want this run to be called?")
-    runname = input(">>> ")
+    runname = raw_input(">>> ")
+
+    if os.path.exists(os.getcwd() + "/{}_HeFESTo_Output_Files/".format(runname)):
+        shutil.rmtree(os.getcwd() + "/{}_HeFESTo_Output_Files/".format(runname))
+    os.mkdir(os.getcwd() + "/{}_HeFESTo_Output_Files/".format(runname))
+
     print("What is the directory name where input directories are located?")
     while True:
-        dirname = input(">>> ")
-        if os.path.exists(dirname):
-            for root, dirs, files in os.walk(dirname, topdown=False):
-                for dir in dirs:
-                    os.mkdir(os.getcwd() + "/HeFESTo_Output_Files/" + dir)
-                    os.mkdir(os.getcwd() + "/HeFESTo_Output_Files/" + dir + "/fort.58")
-                    os.mkdir(os.getcwd() + "/HeFESTo_Output_Files/" + dir + "/fort.59")
-                    os.mkdir(os.getcwd() + "/HeFESTo_Output_Files/" + dir + "/fort.66")
+        inp = raw_input(">>> ")
+        if os.path.exists(os.getcwd() + "/" + inp):
+            dirname = inp
+            print("Found directory: " + os.getcwd() + "/" + dirname)
+            break
+        else:
+            print(os.getcwd() + "/" + inp + " not found!")
+    for root, dirs, files in os.walk(os.getcwd() + "/" + dirname, topdown=False):
+        for dir in dirs:
+            os.mkdir(os.getcwd() + "/{}_HeFESTo_Output_Files/".format(runname) + dir)
+            os.mkdir(os.getcwd() + "/{}_HeFESTo_Output_Files/".format(runname) + dir + "/fort.58")
+            os.mkdir(os.getcwd() + "/{}_HeFESTo_Output_Files/".format(runname) + dir + "/fort.59")
+            os.mkdir(os.getcwd() + "/{}_HeFESTo_Output_Files/".format(runname) + dir + "/fort.66")
 
-                    for rs, ss, fs in os.walk(os.getcwd() + "/" + dir):
-                        for f in fs:
-                            component = None
-                            if "BSP" in f:
-                                component = "BSP"
-                            elif "MORB" in f:
-                                component = "MORB"
-                            cleanup()
-                            shutil.copy(os.getcwd() + "/" + dir + "/" + f, os.getcwd() + "/" + "control")
-                            starcomponent = f.replace("HeFESTo_Infile.txt", "").replace("HeFESTo_Infile.txt", "")
-                            p = subprocess.Popen(os.getcwd() + "/main", stdin=None, stdout=None)
-                            t = Timer(800, p.kill)
-                            t.start()
-                            p.communicate()
-                            t.cancel()
-                            if "fort.58" in os.listdir(os.getcwd()):
-                                shutil.move(os.getcwd() + "/fort.58",
-                                            os.getcwd() + "/HeFESTo_Output_Files/" + dir + "/fort.58/" + "{}.txt".format(
-                                                starcomponent))
-                            if "fort.59" in os.listdir(os.getcwd()):
-                                shutil.move(os.getcwd() + "/fort.58",
-                                            os.getcwd() + "/HeFESTo_Output_Files/" + dir + "/fort.59/" + "{}.txt".format(
-                                                starcomponent))
-                            if "fort.66" in os.listdir(os.getcwd()):
-                                shutil.move(os.getcwd() + "/fort.58",
-                                            os.getcwd() + "/HeFESTo_Output_Files/" + dir + "/fort.66/" + "{}.txt".format(
-                                                starcomponent))
+            for rs, ss, fs in os.walk(os.getcwd() + "/" + dirname + "/" + dir):
+                for f in fs:
+                    component = None
+                    if "BSP" in f:
+                        component = "BSP"
+                    elif "MORB" in f:
+                        component = "MORB"
+                    cleanup()
+                    shutil.copy(os.getcwd() + "/" + dirname + "/" + dir + "/" + f, os.getcwd() + "/" + "control")
+                    starcomponent = f.replace("HeFESTo_Infile.txt", "").replace("HeFESTo_Infile.txt", "")
+                    p = subprocess.Popen(os.getcwd() + "/main", stdin=None, stdout=None)
+                    t = Timer(800, p.kill)
+                    t.start()
+                    p.communicate()
+                    t.cancel()
+                    if "fort.58" in os.listdir(os.getcwd()):
+                        shutil.move(os.getcwd() + "/fort.58",
+                                    os.getcwd() + "/{}_HeFESTo_Output_Files/".format(runname) + dir + "/fort.58/" + "{}.txt".format(
+                                        starcomponent))
+                    if "fort.59" in os.listdir(os.getcwd()):
+                        shutil.move(os.getcwd() + "/fort.58",
+                                    os.getcwd() + "/{}_HeFESTo_Output_Files/".format(runname) + dir + "/fort.59/" + "{}.txt".format(
+                                        starcomponent))
+                    if "fort.66" in os.listdir(os.getcwd()):
+                        shutil.move(os.getcwd() + "/fort.58",
+                                    os.getcwd() + "/{}_HeFESTo_Output_Files/".format(runname) + dir + "/fort.66/" + "{}.txt".format(
+                                        starcomponent))
 
